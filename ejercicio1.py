8eeab5e3724d0c9ba48aa6a430da60bddc90ab85
valuvilla <valuvp@hotmail.com>
2022-10-15 09:28:36 +0200

    comentarios

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,8 +1,18 @@
 import sys
 
+#Definimos una función que controle que el numero introducido sea un numero
+
 def valor(valor):
     while True:
+        #entramos en un bucle infinito
+
+        #pedimos por teclado un valor
         valor=input("Valor: ")
+        
+        """
+        Comprabamos que tal valor es un numero
+        No se saldrá del bucle hasta que se capture un entero
+        """
         try:
             valor=int(valor)
         except:
@@ -15,21 +25,31 @@ def valor(valor):
     return valor
 
 
+#Funcion para evaluar los valores
 
 def Evaluación(valor):
         #Definimos la variables
+
         print("introduce tres valores: ")
+
+        #LLamamos a la función antes definida para evaluar la informacion dada.
         valor1=valor(valor)
         valor2=valor(valor)
-        valor3=valor(valor)   
+        valor3=valor(valor) 
+        """
+        Aplicamos las condiciones de la actividad 
+        """  
         if valor1!=0:
             if valor1<=valor2<=valor3:
                 print("valores ordenados")
             else:
                 print("valores no ordenados")
-                sys.exit()
+               
+        #Al introducir el 0 en primer valor, el sistema nos dará error.        
         else:
-            raise ValueError("{} no es valido".format(valor1))               
+            raise ValueError("{} no es valido".format(valor1))          
+
+
         return valor1, valor2, valor3
 
 
